936077d2f4afba991cc46d986c9efec68d48af9c
aldoushux503 <elnikovnikita4@gmail.com>
2025-02-26 16:28:54 +0100

    Rename a database from db toMovieRecSystem also add MyBatis configuration with MyBatisUtil and config.properties

--- src/main/java/org/solvd/recommendation/Main.java
+++ src/main/java/org/solvd/recommendation/Main.java
@@ -1,7 +1,11 @@
-package org.example;
+package org.solvd.recommendation;
+
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
     }
 }
